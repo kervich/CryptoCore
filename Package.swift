b0a95a12ee88e22bda2b2f3428e276d0a361b68d
Andrey Yurkevich <andrey.yurkevich@kupibilet.ru>
2026-01-05 20:58:48 +0400

    Cleanup

--- Package.swift
+++ Package.swift
@@ -5,30 +5,24 @@ import PackageDescription
 let package = Package(
     name: "CryptoCore",
     platforms: [
-      .macOS(.v10_15),
-      .iOS(.v13),
+        .macOS(.v10_15),
+        .iOS(.v13),
     ],
     products: [
-      .library(name: "CryptoCore", type: .static, targets: ["CryptoCore"]),
+        .library(name: "CryptoCore", type: .static, targets: ["CryptoCore"]),
     ],
     targets: [
         // 👀 C helpers
         .target(name: "keccaktiny"),
         .target(name: "secp256k1", cSettings: [
-          .define("ENABLE_MODULE_ECDH"),
-          .define("ENABLE_MODULE_RECOVERY"),
+            .define("ENABLE_MODULE_ECDH"),
+            .define("ENABLE_MODULE_RECOVERY"),
         ]),
-
-        // 🎯 Target -- Base58
-        .target(name: "Base58"),
-
         // 🎯 Target -- CryptoCore
         .target(name: "CryptoCore", dependencies: [
-            "keccaktiny", 
-            "secp256k1",
-            "Base58",
+            "keccaktiny",
+            "secp256k1"
         ]),
-
         // Test -- CryptoCore
         .testTarget(name: "CryptoCoreTests", dependencies: [
             "CryptoCore"
--- Tests/CryptoCoreTests/CryptoCoreTests.swift
+++ Tests/CryptoCoreTests/CryptoCoreTests.swift
@@ -2,9 +2,8 @@ import XCTest
 import CryptoCore
 
 final class CryptoCoreTests: XCTestCase {
-  func testCryptoCore() throws {
-    print(try! Data(hexString: "30").hexString)
-    print("Hello")
-  }
+    func testCryptoCore() throws {
+        print("Hello")
+    }
 }
 
